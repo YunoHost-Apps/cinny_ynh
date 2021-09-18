bbd5fe038a28cf49d8a35250c44721d4dde5ec6f
Félix Piédallu <felix@piedallu.me>
2021-09-18 22:08:45 +0200

    Ensure Cinny is always installed on a domain root

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,6 +11,14 @@ nodejs_version=14
 # PERSONAL HELPERS
 #=================================================
 
+ynh_webpath_is_domain_root () {
+    local -A args_array=( [p]=path_url= )
+    local path_url
+    ynh_handle_getopts_args "$@"
+
+    [[ "$path_url" != "/" ]] && [[ "$path_url" != "" ]]
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
