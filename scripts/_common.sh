81dbc1c612ee85b5960e41b3c986e5f8e0858b6d
Félix Piédallu <felix@piedallu.me>
2021-11-05 14:48:40 +0100

    Allow installing in subdir again

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,14 +8,6 @@
 # PERSONAL HELPERS
 #=================================================
 
-ynh_webpath_is_domain_root () {
-    local -A args_array=( [p]=path_url= )
-    local path_url
-    ynh_handle_getopts_args "$@"
-
-    [[ "$path_url" != "/" ]] && [[ "$path_url" != "" ]]
-}
-
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
