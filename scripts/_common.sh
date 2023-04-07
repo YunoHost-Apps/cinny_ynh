23c871d8a90a69ced533a10a5b7c2092649057d9
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-07 10:21:01 +0200

    add default server

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,19 @@
 # PERSONAL HELPERS
 #=================================================
 
+cinny_install_config_file() {
+    # This helper detects if default_home_server is matrix.org to prevent duplicates in the list.
+
+    # Add a Json element in the config file
+    if [[ "$default_home_server" == "matrix.org" ]]; then
+        default_home_server_list=""
+    else
+        default_home_server_list="\"$default_home_server\",\n    "
+    fi
+
+    ynh_add_config --template="../conf/config.json" --destination="$install_dir/config.json"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
